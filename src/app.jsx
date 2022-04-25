d7fefb9537d88c2e2abc099be34dd1e9368a5d55
Han <ahsj22@naver.com>
2022-04-25 12:36:25 +0900

    First Point(Navbar)

--- src/app.jsx
+++ src/app.jsx
@@ -1,7 +1,31 @@
 import "./app.css";
+import { Routes, Route } from "react-router-dom";
+import ProductAll from "./pages/productAll";
+import ProductDetail from "./pages/productDetail";
+import Login from "./pages/login";
+import Navbar from "./components/navbar";
+
+// 1. 전체상품, 로그인, 상세 페이지
+// 1-1. 네비게이션 바 만들기
+// 2. 전체상품 페이지를 누르면, 전체 상품을 볼 수 있다.
+// 3. 로그인 버튼을 누르면, 로그인 페이직 나온다.
+// 4. 상품디테일 버튼을 누르면, 로그인이 안된경우, 로그인 페이지가 나온다.
+// 5. 로그인이 된 경우, 상품 디테일 페이지를 볼 수 있다.
+// 6. 로그아웃 버튼을 누르면, 로그아웃 된다.
+// 7. 로그아웃 되면, 상품 디테일 페이지를 볼 수 없고, 다시 로그인 페이지가 보인다.
+// 8. 상품을 검색할 수 있다.
 
 function App() {
-  return <h1>Han :)</h1>;
+  return (
+    <div>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<ProductAll />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/product/:id" element={<ProductDetail />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/navbar.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUser } from "@fortawesome/free-regular-svg-icons";
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
+
+const Navbar = () => {
+  const menuList = ["남성", "Divided", "여성", "신생아 / 유아", "지속가능성"];
+
+  return (
+    <div>
+      <div className="login-btn">
+        <FontAwesomeIcon icon={faUser} />
+        <div>로그인</div>
+      </div>
+      <div className="nav-section">
+        <img width={150} src="/images/logo.png" />
+      </div>
+      <div className="menu-area">
+        <ul className="menu-list">
+          {menuList.map((menu) => (
+            <li>{menu}</li>
+          ))}
+        </ul>
+        <div className="menu-search">
+          <FontAwesomeIcon icon={faSearch} />
+          <input type="text" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
